fd0741526158eeb16fb409a4d9944bf77cf36789
PearlFoe <itknml@gmail.com>
2024-10-28 14:38:21 +0300

    Set db connection url for migrations

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,12 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from src.settings import Settings
+
+env = Settings()
+config = context.config
+
+config.set_main_option("sqlalchemy.url", str(env.db_dsn))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
