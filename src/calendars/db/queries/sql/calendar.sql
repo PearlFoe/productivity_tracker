3fee331eb061cafdae4b5c571a514c08b7353ac6
PearlFoe <itknml@gmail.com>
2024-10-31 13:44:16 +0300

    Add sql queries for calendar logic

---
+++ src/calendars/db/queries/sql/calendar.sql
@@ -0,0 +1,24 @@
+-- name: create_calendar!
+INSERT INTO pt.calendar (user_id, name, description, timezone)
+VALUES (
+    (
+        SELECT u.id 
+        FROM pt.user as u
+        WHERE u.tg_id = :tg_id
+    ),
+    :name,
+    :description,
+    :timezone
+)
+RETURNING pt.calendar.id;
+
+-- name: update_calendar_category!
+UPDATE pt.calendar (category)
+VALUES (
+    (
+        SELECT с.id 
+        FROM pt.calendar_category as с
+        WHERE c.name = :category
+    )
+)
+WHERE pt.calendar.id = :calendar_id;
