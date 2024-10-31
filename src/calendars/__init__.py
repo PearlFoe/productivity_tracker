1e89ab37b668950009bb14f8da907c3531f0d5e0
PearlFoe <itknml@gmail.com>
2024-10-31 09:38:33 +0300

    Init modules

---
+++ src/calendars/__init__.py
@@ -0,0 +1,19 @@
+from aiogram import Router
+
+from src.settings import Settings
+from .routers import router
+from .containers import CalendarContainer
+
+
+async def _get_container(settings: Settings) -> CalendarContainer:
+    container = CalendarContainer()
+    container.env.from_dict(settings.model_dump())
+    await container.init_resources()
+    return container
+
+
+async def get_router(settings: Settings) -> Router:
+    _router = Router()
+    _router.container = await _get_container(settings)
+    _router.include_router(router)
+    return _router
--- src/main.py
+++ src/main.py
@@ -5,13 +5,17 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
 
 from src.settings import Settings
-from src.start import get_router as get_start_router
 from src.bot.containers import BotContainer
 from src.bot.commands import COMMANDS
+from src.start import get_router as get_start_router
+from src.calendars import get_router as get_calendar_router
 
 
 async def _include_routers(dp: Dispatcher, settings: Settings) -> None:
-    routers_getters = (get_start_router,)
+    routers_getters = (
+        get_start_router,
+        get_calendar_router,
+    )
 
     for getter in routers_getters:
         router = await getter(settings)
