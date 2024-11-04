098c30c5e4ae3e77e9c4b7c9daf693a9a0cbcb7f
PearlFoe <itknml@gmail.com>
2024-11-04 14:20:09 +0300

    Update calendar models

--- src/calendars/models/calendars.py
+++ src/calendars/models/calendars.py
@@ -7,7 +7,6 @@ class Calendar(BaseModel):
     google_id: str
     name: str
     timezone: str
-    primary: bool
     id: UUID | None = None
     description: str | None = None
     category: str | None = None
--- src/calendars/models/clients/calendars.py
+++ src/calendars/models/clients/calendars.py
@@ -4,6 +4,5 @@ from pydantic import BaseModel, Field
 class Calendar(BaseModel):
     id: str
     summary: str
-    description: str
     timezone: str = Field(alias="timeZone")
-    primary: bool
+    description: str | None = Field(None)
