d6eaac3c218b4c933a107c7307e42c0d042276e5
PearlFoe <itknml@gmail.com>
2024-11-05 11:42:20 +0300

    Add reference data loading

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-uv run alembic upgrade head  # run migrations
+uv run alembic upgrade head
+
+echo -e "\n\nLoading reference data.\n\n"
+uv run python3 data/load.py  --file data/sql/reference_data.sql
+
 uv run python src/main.py
--- src/calendars/constants/calendar_category.py
+++ src/calendars/constants/calendar_category.py
@@ -3,4 +3,4 @@ from enum import StrEnum, auto
 
 class CalendarCategory(StrEnum):
     WORK = auto()
-    ENTETAINMENT = auto()
+    ENTERTAINMENT = auto()
