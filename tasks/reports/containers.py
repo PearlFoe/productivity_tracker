de9cdafb7b3cbe84b906a53c31d9dcdab35048a4
PearlFoe <itknml@gmail.com>
2025-02-07 10:45:47 +0300

    Update report container

--- tasks/reports/containers.py
+++ tasks/reports/containers.py
@@ -1,5 +1,12 @@
+from concurrent.futures import ThreadPoolExecutor
+from os import path
+
 from dependency_injector import containers, providers
 
+from tasks.core.db.utils import init_db_connection_pool, load_queries
+
+from .db.queries.builders import ReportQueryBuilder
+from .db.repositories import ReportRepository
 from .services.report_builders import ReportBuildingService
 
 
@@ -11,6 +18,37 @@ class ReportContainer(containers.DeclarativeContainer):
 
     env = providers.Configuration()
 
+    report_unloaded_qb = providers.Singleton(ReportQueryBuilder)
+
+    _report_queries_path = providers.Callable(
+        path.join,
+        env.project_dir,
+        "tasks/calendars_statistics/db/queries/sql/calendar.sql",
+    )
+
+    report_qb = providers.Factory(
+        load_queries,
+        builder=report_unloaded_qb,
+        path=_report_queries_path,
+    )
+
+    db_connection_pool = providers.Resource(
+        init_db_connection_pool,
+        dsn=env.db_dsn,
+    )
+
+    report_repository = providers.Factory(
+        ReportRepository,
+        pool=db_connection_pool,
+        queries=report_qb,
+    )
+
+    thread_pool = providers.Factory(
+        ThreadPoolExecutor,
+    )
+
     report_building_service = providers.Factory(
         ReportBuildingService,
+        statistics=report_repository,
+        pool=thread_pool,
     )
