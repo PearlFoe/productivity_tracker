e40721878b71f606b01672b3fd8ce1d52eb5eaf9
PearlFoe <itknml@gmail.com>
2025-02-07 20:26:16 +0300

    Configure interpolation

--- tasks/reports/services/charts/chart_sets.py
+++ tasks/reports/services/charts/chart_sets.py
@@ -15,10 +15,29 @@ class BaseChartSet(BaseModel):
     )
 
 
+_INTERPOLATION_PARAMETERS = {
+    "interpolate": "hermite",
+    "interpolation_parameters": {
+        "type": "monotone",
+    },
+}
+
+
 class UserReportChartSet(BaseChartSet):
     template: str = "tasks/reports/templates/user_report.html"
     charts: dict[str, BaseChart] = {
-        "time_per_calendar": TimePerCalendarChart(chart=Line(style=DEFAULT_STYLE)),
-        "time_total": TimeTotalChart(chart=StackedLine(style=DEFAULT_STYLE, fill=True)),
+        "time_per_calendar": TimePerCalendarChart(
+            chart=Line(
+                style=DEFAULT_STYLE,
+                **_INTERPOLATION_PARAMETERS,
+            )
+        ),
+        "time_total": TimeTotalChart(
+            chart=StackedLine(
+                style=DEFAULT_STYLE,
+                fill=True,
+                **_INTERPOLATION_PARAMETERS,
+            )
+        ),
         "percent_per_calendar": PercentPerCalendarChart(chart=Pie(style=DEFAULT_STYLE)),
     }
