dba8f31058f395b4bbc87f7b77b35e505d225360
PearlFoe <itknml@gmail.com>
2025-02-11 15:20:54 +0300

    Add flow for report build scheduling

--- tasks/reports/flows.py
+++ tasks/reports/flows.py
@@ -13,8 +13,8 @@ CONTAINER = ReportContainer()
 CONTAINER.env.from_dict(SETTINGS.model_dump())
 
 
-@flow(name="build_weekly_report")
-async def build_weekly_report(filter: ReportFiler) -> None:
+@flow(name="build_report")
+async def build_report(filter: ReportFiler) -> None:
     report_builder: ReportBuildingService = await CONTAINER.report_building_service()
     report_distributor: ReportDistributionService = await CONTAINER.report_distribution_service()
 
@@ -26,3 +26,20 @@ async def build_weekly_report(filter: ReportFiler) -> None:
         user_id=filter.user_id,
         report=report,
     )
+
+
+@flow(name="schedule_report_building")
+async def schedule_report_building() -> None:
+    report_distributor: ReportDistributionService = await CONTAINER.report_distribution_service()
+
+    start, end = report_distributor.get_weekly_report_dates()
+    users = await report_distributor.get_users_to_send_report()
+
+    for user_id in users:
+        filter = ReportFiler(
+            user_id=user_id,
+            start=start,
+            end=end,
+        )
+
+        await build_report(filter)
